1dcf5005434438252809a2fd5d7f650b1740cc01
Tarun Velagala <tarun.kumar.reddy.velagala@sap.com>
2026-03-31 18:03:36 +0530

    (fix): Remove Onboarding Screen Feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
-import 'features/onboarding/presentation/providers/onboarding_providers.dart';
-import 'l10n/app_localizations.dart';
 import 'router/app_router.dart';
 
 void main() {
@@ -15,33 +12,12 @@ class KhataMitraApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final themeMode = ref.watch(themeModeProvider);
-    final language = ref.watch(selectedLanguageProvider);
-
     return MaterialApp.router(
       title: 'KhataMitra',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
-      themeMode: themeMode,
       routerConfig: appRouter,
-      locale: Locale(language.code),
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'),
-        Locale('hi'),
-        Locale('kn'),
-        Locale('ta'),
-        Locale('bn'),
-        Locale('mr'),
-        Locale('ml'),
-        Locale('te'),
-      ],
     );
   }
 }
